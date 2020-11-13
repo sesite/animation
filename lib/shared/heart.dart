427714daca952c9b44f239668313bee72bed85e3
SVA <mr.supervit@gmail.com>
2020-11-13 08:24:52 +0300

    add animation hearth

--- lib/shared/heart.dart
+++ lib/shared/heart.dart
@@ -5,15 +5,77 @@ class Heart extends StatefulWidget {
   _HeartState createState() => _HeartState();
 }
 
-class _HeartState extends State<Heart> {
+class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
+  bool isRed = false;
+
+  AnimationController _animcontrolhearth;
+  Animation<Color> _colorAnimation;
+  Animation<double> _sizeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animcontrolhearth = AnimationController(
+      duration: Duration(milliseconds: 300),
+      vsync: this,
+    );
+
+    _colorAnimation = ColorTween(
+      begin: Colors.grey[400],
+      end: Colors.red,
+    ).animate(_animcontrolhearth);
+
+    _sizeAnimation = TweenSequence(
+      <TweenSequenceItem<double>>[
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 30, end: 50),
+          weight: 50,
+        ),
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 50, end: 30),
+          weight: 30,
+        ),
+      ],
+    ).animate(_animcontrolhearth);
+
+    _animcontrolhearth.addListener(() {});
+
+    _animcontrolhearth.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        setState(() {
+          isRed = true;
+        });
+      }
+      if (status == AnimationStatus.dismissed) {
+        setState(() {
+          isRed = false;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _animcontrolhearth.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(
-        Icons.favorite,
-        size: 32,
-      ),
-      onPressed: () {},
+    return AnimatedBuilder(
+      builder: (BuildContext context, _) {
+        return IconButton(
+          icon: Icon(
+            Icons.favorite,
+            color: _colorAnimation.value,
+            size: _sizeAnimation.value,
+          ),
+          onPressed: () {
+            isRed ? _animcontrolhearth.reverse() : _animcontrolhearth.forward();
+          },
+        );
+      },
+      animation: _animcontrolhearth,
     );
   }
 }
